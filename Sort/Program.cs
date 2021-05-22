c52824199f5d42a36e7bef8aea447a3ba4695eb0
Panlak <55487154+Panlak@users.noreply.github.com>
2021-05-22 18:42:19 +0300

    update

--- Sort/Program.cs
+++ Sort/Program.cs
@@ -13,66 +13,66 @@ namespace BubbleSort
 		{
 			Console.OutputEncoding = System.Text.Encoding.Unicode;
 			Console.InputEncoding = System.Text.Encoding.Unicode;
-			Console.WriteLine("Сортування бульбальшкою");
+
+			Console.WriteLine("''Найпростіші алгоритми сортування''".Pastel("#fbff00"));
+			Console.WriteLine("Сортування бульбальшкою".Pastel("#00ffff"));
 			Console.Write("Початковий масив:\t");
-			BigInteger[] someArray = new BigInteger[] { 1, 2, 4, 3, 8, 5, 7, 6, 9, 0 };
+			BigInteger[] someArray = randomArrey(20);
 			Print(someArray,false);
-			Console.WriteLine();
 			var sw1 = new Stopwatch();
 			sw1.Start();
 			BubbleSortAlg.Bubble_Sort(someArray);
 			sw1.Stop();
 			Console.Write("Відсортований  масив:\t");
 			Print(someArray,true);
-			Console.WriteLine($"Time Spent: {sw1.Elapsed}");
+			Console.WriteLine($"Time Spent: {sw1.Elapsed}".Pastel("#ffbb00"));
 			Console.WriteLine("-----------------------------------------------------------------------------");
 			////////////////////////////////////////////////////////////
-			Console.WriteLine("Сортування вибором");
-			BigInteger[] someArraySelection = new BigInteger[] { 1, 2, 4, 3, 8, 5, 7, 6, 9, 0 };
+			Console.WriteLine("Сортування вибором".Pastel("#00ffff"));
+			BigInteger[] someArraySelection = randomArrey(20);
 			Console.Write("Початковий масив:\t");
 			Print(someArray, false);
-			Console.WriteLine();
 			var sw2 = new Stopwatch();
 			sw2.Start();
 			ChoiseAlg.ViborSort(someArraySelection);
 			sw2.Stop();
 			Console.Write("Відсортований  масив:\t");
 			Print(someArray,true);
-			Console.WriteLine($"Time Spent: {sw2.Elapsed}");
-			Console.WriteLine();
+			Console.WriteLine($"Time Spent: {sw2.Elapsed}".Pastel("#ffbb00"));
+			
 			Console.WriteLine("-----------------------------------------------------------------------------");
 			////////////////////////////////////////////////////////////
-			Console.WriteLine("Сортування вставкою");		
-			someArray = new BigInteger[] { 40, 30, 10, 70, 50, 20, 60, };
+			Console.WriteLine("Сортування вставкою".Pastel("#00ffff"));		
+			someArray = randomArrey(20);
 			Console.Write("Початковий масив:\t");
 			Print(someArray,false);
-			Console.WriteLine();
 			var sw3 = new Stopwatch();
 			sw3.Start();
 			InsertSortAlg.InsertionSort(someArray);
 			sw3.Stop();
 			Console.Write("Відсортований  масив:\t");
 			Print(someArray,true);
-			Console.WriteLine($"Time Spent: {sw3.Elapsed}");
-			Console.WriteLine();
-			Console.WriteLine("-----------------------------------------------------------------------------");
+			Console.WriteLine($"Time Spent: {sw3.Elapsed}".Pastel("#ffbb00"));
+			Console.WriteLine("-----------------------------------------------------------------------------".Pastel("#00ff59"));
+			Console.WriteLine("Наступна Лаблаторна Вдосконалені алгоритми сортування".Pastel("#fbff00"));
+			Console.WriteLine("-----------------------------------------------------------------------------".Pastel("#00ff59"));
+			
 			//////////////////////////////////////////////////////////////////////////////////////////////////
-			Console.WriteLine("Сортування Шела");
-			someArray = new BigInteger[] { 40, 30, 10, 70, 50, 20, 60, };
+
+			Console.WriteLine("Сортування Шела".Pastel("#00ffff"));
+			someArray = randomArrey(20);
 			Console.Write("Початковий масив:\t");
 			Print(someArray, false);
 			var sw4 = new Stopwatch();
 			sw4.Start();
 			ShellSort.Shell(someArray);
 			sw4.Stop();
-			Console.WriteLine();
 			Console.Write("Відсортований  масив:\t");
 			Print(someArray,true);
-			Console.WriteLine($"Time Spent: {sw4.Elapsed}");
-			Console.WriteLine();
+			Console.WriteLine($"Time Spent: {sw4.Elapsed}".Pastel("#ffbb00"));
 			Console.WriteLine("-----------------------------------------------------------------------------");
-			Console.WriteLine("Порозрядне сортування");
-			someArray = new BigInteger[] { 39, 47, 54, 59, 34, 41, 32 };
+			Console.WriteLine("Порозрядне сортування".Pastel("#00ffff"));
+			someArray = randomArrey(20);
 			Console.Write("Початковий масив:\t");
 			Print(someArray, false);
 			var sw5 = new Stopwatch();
@@ -80,13 +80,11 @@ namespace BubbleSort
 			BitwiseSorting.sorting(someArray,10,2);
 			sw5.Stop();
 			Console.Write("Відсортований  масив:\t");
-			Print(someArray, true);
-			Console.WriteLine("");
-			Console.WriteLine($"Time Spent: {sw5.Elapsed}");
-			Console.WriteLine();
+			Print(someArray, true);		
+			Console.WriteLine($"Time Spent: {sw5.Elapsed}".Pastel("#ffbb00"));	
 			Console.WriteLine("-------------------------------------------------------------------------------");
-			Console.WriteLine("Швидке сортування");
-			someArray = new BigInteger[] { 39, 47, 54, 59, 34, 41, 32 };
+			Console.WriteLine("Швидке сортування".Pastel("#00ffff"));
+			someArray = randomArrey(20);
 			Console.Write("Початковий масив:\t");
 			Print(someArray, false);
 			var sw6 = new Stopwatch();
@@ -95,12 +93,10 @@ namespace BubbleSort
 			Console.Write("Відсортований  масив:\t");
 			Print(someArray, true);
 			sw6.Stop();
-			Console.WriteLine("");
-			Console.WriteLine($"Time Spent: {sw6.Elapsed}");
-			Console.WriteLine();
+			Console.WriteLine($"Time Spent: {sw6.Elapsed}".Pastel("#ffbb00"));
 			Console.WriteLine("-------------------------------------------------------------------------------");
-			Console.WriteLine("Сортування підрахунком");
-			someArray = new BigInteger[] { 39, 47, 54, 59, 34, 41, 32,36,46 };
+			Console.WriteLine("Сортування підрахунком".Pastel("#00ffff"));
+			someArray = randomArrey(20);
 			Console.Write("Початковий масив:\t");
 			Print(someArray, false);
 			var sw8 = new Stopwatch();
@@ -109,25 +105,21 @@ namespace BubbleSort
 			sw8.Stop();
 			Console.Write("Відсортований  масив:\t");
 			Print(someArray, true);
-			Console.WriteLine($"Time Spent: {sw8.Elapsed}");
-			Console.WriteLine();
+			Console.WriteLine($"Time Spent: {sw8.Elapsed}".Pastel("#ffbb00"));
 			Console.WriteLine("-------------------------------------------------------------------------------");
+		}
 
-
-
-
+		public static BigInteger[] randomArrey(int max)
+		{
 			Random random = new Random();
-			BigInteger[] arrey = new BigInteger[10000];
-			var sw7 = new Stopwatch();			
+			BigInteger[] arrey = new BigInteger[max];
 			for (int i = 0; i < arrey.Length; i++)
 			{
-				arrey[i] = random.Next(10,100);
+				arrey[i] = random.Next(1, max);
 			}
-			sw7.Start();
-			InsertSortAlg.InsertionSort(arrey);
-			sw7.Stop();
-			Console.WriteLine($"Time Spent: {sw7.Elapsed}");
+			return arrey;
 		}
+
 		public static void Print(BigInteger[] arrey,bool color)
 		{
 			for (int i = 0; i < arrey.Length; i++)
